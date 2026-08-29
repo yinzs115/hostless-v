ed925598aaf05d1b16b164c41141ef5284ccc2cc
KJGX66F <github@kjgx.eu.org>
2026-08-29 16:42:04 +0800

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const { WebSocketServer, createWebSocketStream } = require('ws');
 
 // 1. 读取环境变量与配置
 const PORT = process.env.PORT || 8000;
-const UUID = (process.env.UUID || '9c5be7af-d13a-4a7f-9bd8-d0a13e99bcf5').replace(/-/g, '').toLowerCase();
+const UUID = (process.env.UUID || '').replace(/-/g, '').toLowerCase();
 
 // 2. 先创建 HTTP 服务（提供健康检查，防止平台杀死进程）
 const server = http.createServer((req, res) => {
